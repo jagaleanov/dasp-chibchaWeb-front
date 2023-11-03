deda5adae046b13fce85fdd1d03a3478c6596785
jagaleanov <54491111+jagaleanov@users.noreply.github.com>
2023-11-02 23:44:59 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 export const SPA_CONFIG = {
     BASE_URL: isProduction ? 'https://jagaleanov.github.io/dasp-chibchaWeb' : 'http://chibchaweb.com:8082',
-    API_URL: isProduction ? 'https://chibchaweb.infinityfreeapp.com/api' : 'https://chibchaweb.com:444/api',
+    API_URL: isProduction ? 'http://18.228.214.15/api' : 'https://chibchaweb.com:444/api',
 };
